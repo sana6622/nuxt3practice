80b8c45fe38353909e66f334ee06df0820d13f5e
sana <sana6622@gmail.com>
2024-06-11 00:25:29 +0800

    初始檔案 已加入 components composables layouts plugins

---
+++ layouts/default.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>
+    Layout: 這是layout111
+    <slot />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>
+    以下是page -> index頁
+  </div>
+</template>
+
+<style scoped></style>
